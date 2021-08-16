af0168aae31e2a6b2ab896aa47c603f6bcae2215
Pobarobashka <pobarobashka@tut.by>
2021-08-16 16:36:25 +0300

    lesson2

--- js/script.js
+++ js/script.js
@@ -6,4 +6,11 @@ window.addEventListener('DOMContentLoaded',()=>{
     document.querySelector('.menu_close').addEventListener('click',()=>{
         document.querySelector('.menu').classList.remove('active');
     })
+    const percent = document.querySelectorAll('.skill_progress_percent'),
+        progress = document.querySelectorAll('.skills_progress_bar span');
+    (function (){
+        percent.forEach((item,number)=>{
+            progress[number].style.width=item.textContent;
+        })
+    }());
 });
